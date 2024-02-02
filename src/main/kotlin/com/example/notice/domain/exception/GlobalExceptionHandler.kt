25b204a875f5a660290872103f7b6428638328b2
hjm <gks777777@gmail.com>
2024-02-03 04:33:40 +0900

    handleIllegalArgumentException

--- src/main/kotlin/com/example/notice/domain/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/notice/domain/exception/GlobalExceptionHandler.kt
@@ -43,5 +43,12 @@ class GlobalExceptionHandler {
             .body(ErrorResponse(e.message))
     }
 
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse(e.message))
+    }
+
 
 }
